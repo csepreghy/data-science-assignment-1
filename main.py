65552f21af814da70ea06532060b5fc673184ae8
Andrew Chepreghy <andris788@gmail.com>
2019-02-14 14:06:38 +0100

    mean fev1 score for smokers and non-smokers

--- main.py
+++ main.py
@@ -5,4 +5,30 @@ from matplotlib import style
 style.use('fivethirtyeight')
 
 smoking_data = np.loadtxt('smoking.txt', delimiter="\t")
-print(smoking_data)
+
+# age – a positive integer (years)
+# FEV1 – a continuous valued measurement(liter)
+# height – a continuous valued measurement(inches) 
+# gender – binary(female: 0, male: 1)
+# smoking status – binary(non-smoker: 0, smoker: 1) 
+# weight – a continuous valued measurement(kg)
+
+smokers = []
+nonsmokers = []
+
+for person in smoking_data:
+  if person[4] == 0:
+    nonsmokers.append(person)
+  elif person[4] == 1:
+    smokers.append(person)
+
+print(np.array(smokers))
+smokers = np.array(smokers)
+nonsmokers = np.array(nonsmokers)
+
+smokers_avg_FEV1 = np.mean(smokers[:,1])
+nonsmokers_avg_FEV1 = np.mean(nonsmokers[:, 1])
+
+print("Smokers' FEV1 score: ", str(smokers_avg_FEV1))
+print("Non-smokers' FEV1 score: ", str(nonsmokers_avg_FEV1))
+
