3282584f15643cdb675cf63077ca27bdcac622e4
Andrew Chepreghy <andris788@gmail.com>
2019-02-16 21:07:45 +0100

    created scatter plot

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 from scipy import stats
+from decimal import *
+from matplotlib import rcParams
+import matplotlib.font_manager
+
+rcParams['font.sans-serif'] = ['Arial']
+
+
+
+
+print([f.name for f in matplotlib.font_manager.fontManager.afmlist])
+
+getcontext().prec = 26
+getcontext().Emin = -9999999999999
 
 plt.style.use('dark_background')
 
@@ -73,10 +86,14 @@ for patch, color in zip(bplot['boxes'], colors):
 
 ax.set_ylabel('FEV1 score')
 ax.set_title('FEV1 scores for smokers\nand non-smokers')
+ax.legend((bplot['boxes']), ('Smokers', 'Non-smokers'),
+          fontsize='medium', loc=2, facecolor='#282D33')
 
 plt.subplots_adjust(top=0.85)
 plt.grid(True, color='#444444')
-plt.show()
+
+
+# Exercise 3
 
 variance_smokers = smokers_FEV1.var(ddof=1)
 variance_nonsmokers = nonsmokers_FEV1.var(ddof=1)
@@ -89,7 +106,37 @@ print(standard_deviation)
 
 # t = (smokers_FEV1.mean() - nonsmokers_FEV1.mean())/(standard_deviation*np.sqrt(2/N))
 
+alpha = 0.05
+
 t, p = stats.ttest_ind(smokers_FEV1, nonsmokers_FEV1, equal_var=False)
 
 print('t is: ', t)
 print('p is: ', p)
+
+if p < alpha:
+  print('Null hypothesis is rejected. The likeliness of it being true is: ' + str(p*100) + '%')
+
+if p > alpha:
+  print('Null hypothesis is acceoted.')
+
+smokers_x = smokers[:,0]
+smokers_y = smokers[:, 1]
+
+nonsmokers_x = nonsmokers[:, 0]
+nonsmokers_y = nonsmokers[:, 1]
+
+fig, ax = plt.subplots()
+fig.patch.set_facecolor(dark_background)
+ax.set_facecolor(dark_background)
+
+ax.set_ylabel('FEV1 score')
+ax.set_xlabel('Age')
+ax.set_title('FEV1 scores among different agegroups')
+
+ax.scatter(nonsmokers_x, nonsmokers_y, linewidths=0.25, alpha=0.5, c='#4FB99F')
+ax.scatter(smokers_x, smokers_y, linewidths=0.25, alpha=0.5, c='#F2B134')
+plt.grid(True, color='#444444')
+
+plt.legend(('Non-smokers', 'Smokers'), facecolor='#282D33')
+
+plt.show()
